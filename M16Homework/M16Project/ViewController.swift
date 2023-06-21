9c4741a6bebeed9d8ebb90a95f297b23032ca88e
Skillbox <it@skillbox.ru>
2023-06-21 19:30:01 +0300

    Add lesson

---
+++ M16Homework/M16Project/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  M16Project
+//
+//  Created by sotskiy on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = "Student name"
+    }
+
+
+}
+
