76ed6c332e2acf7f49ddf9ad81952da039fe2bcc
Ivan Konishchev <kispaj@mail.ru>
2023-08-02 20:14:32 +0300

    Added change: Create HW_20 project

---
+++ M20Homework/HW_20/HW_20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_20
+//
+//  Created by Ivan Konishchev on 02.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
